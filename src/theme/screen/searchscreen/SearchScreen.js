f5b3d00de476babe65155b8cc636064a960814e4
Thanh-Minh <41420911+Thanh-Minh@users.noreply.github.com>
2020-01-11 09:16:54 +0700

    Update home & seach

--- src/theme/screen/searchscreen/SearchScreen.js
+++ src/theme/screen/searchscreen/SearchScreen.js
@@ -55,120 +55,6 @@ class SearchScreen extends Component {
     render() {
         return (
             <View style={{flex: 1, flexDirection: 'column'}}>
-                <Modal
-                    animationType={'slide'}
-                    transparent={true}
-                    visible={this.state.isVisible}
-                    onRequestClose={() => {
-                        this.setState({isVisible: false})
-                    }}>
-                    <View style={{
-                        flex: 1,
-                        justifyContent: 'flex-end',
-                        alignItems: 'center',
-                        backgroundColor: 'rgba(0,0,0,0.3)'
-                    }} {...this.panResponder.panHandlers}>
-                        <View style={{
-                            width: 400,
-                            height: 340,
-                            backgroundColor: '#fff', paddingLeft: 30, paddingRight: 30
-                        }}>
-                            <View style={{flexDirection: 'row', marginTop: 20}}>
-                                <View style={{width: '33%', alignItems: 'center', justifyContent: 'center',}}>
-                                    <Text>Adult</Text>
-                                    <Text style={{color: '#9C9C9C'}}>Age 12+</Text>
-                                </View>
-                                <View style={{width: '33%', alignItems: 'center', justifyContent: 'center',}}>
-                                    <Text>Child</Text>
-                                    <Text style={{color: '#9C9C9C'}}>Age 2-11</Text>
-                                </View>
-                                <View style={{width: '33%', alignItems: 'center'}}>
-                                    <Text>infant</Text>
-                                    <Text style={{color: '#9C9C9C'}}>Bellow Age 2</Text>
-                                </View>
-                            </View>
-                            <View style={{flexDirection: 'row', marginTop: 20}}>
-                                <View style={{width: '33%', alignItems: 'center', justifyContent: 'center',}}>
-                                    <Text style={{color: '#9C9C9C', fontSize: 20}}>0</Text>
-                                </View>
-                                <View style={{width: '33%', alignItems: 'center', justifyContent: 'center',}}>
-                                    <Text style={{color: '#9C9C9C', fontSize: 20}}>0</Text>
-                                </View>
-                                <View style={{width: '33%', alignItems: 'center'}}>
-                                    <Text style={{color: '#9C9C9C', fontSize: 20}}>0</Text>
-                                </View>
-                            </View>
-                            <View style={{flexDirection: 'row', marginTop: 20}}>
-                                <View style={{
-                                    width: '33%',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    flexDirection: 'row'
-                                }}>
-                                    <Image style={{width: 25, height: 25,}}
-                                           source={require('../../../accset/images/Icon/calendar.png')}/>
-                                    <Text style={{color: '#9C9C9C', fontSize: 20, margin: 5, marginRight: 30}}>0</Text>
-                                </View>
-                                <View style={{
-                                    width: '33%',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    flexDirection: 'row',
-                                }}>
-                                    <Image style={{width: 25, height: 25,}}
-                                           source={require('../../../accset/images/Icon/calendar.png')}/>
-                                    <Text style={{color: '#9C9C9C', fontSize: 20, margin: 5, marginRight: 30}}>0</Text>
-                                </View>
-                                <View style={{
-                                    width: '33%',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    flexDirection: 'row'
-                                }}>
-                                    <Image style={{width: 25, height: 25,}}
-                                           source={require('../../../accset/images/Icon/calendar.png')}/>
-                                    <Text style={{color: '#9C9C9C', fontSize: 20, margin: 5, marginRight: 30}}>0</Text>
-                                </View>
-                            </View>
-                            <View style={{flexDirection: 'row', marginTop: 20}}>
-                                <View style={{width: '33%', alignItems: 'center', justifyContent: 'center',}}>
-                                    <Text style={{color: '#9C9C9C', fontSize: 20}}>0</Text>
-                                </View>
-                                <View style={{width: '33%', alignItems: 'center', justifyContent: 'center',}}>
-                                    <Text style={{color: '#9C9C9C', fontSize: 20}}>0</Text>
-                                </View>
-                                <View style={{width: '33%', alignItems: 'center'}}>
-                                    <Text style={{color: '#9C9C9C', fontSize: 20}}>0</Text>
-                                </View>
-                            </View>
-                            <View style={{flexDirection: 'row', marginTop: 30, borderTopWidth: 1}}>
-                                <TouchableOpacity style={{
-                                    width: '40%',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    borderColor:'red',
-                                    borderWidth: 1,
-                                    margin: 20,
-                                    borderRadius:5,
-                                    borderBlockEndColor:'red'
-                                }}>
-                                    <Text style={{color: 'red' ,padding: 10,fontSize:18}}>Reset</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity style={{
-                                    width: '40%',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    borderWidth: 1,
-                                    borderColor:'red',
-                                    margin: 20,
-                                    borderRadius:5,
-                                    backgroundColor:'red'}}>
-                                    <Text style={{color: '#fff', padding: 10, fontSize:18}}>0</Text>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-                    </View>
-                </Modal>
                 <View style={{margin: 10}}>
                     <View style={{flexDirection: 'row'}}>
                         <View>
@@ -180,7 +66,7 @@ class SearchScreen extends Component {
                             }} style={{flexDirection: 'row'}}>
                                 <View style={{width: 20, height: 25, justifyContent: 'flex-end'}}>
                                     <Image style={{width: 20, height: 20,}}
-                                           source={require('../../../accset/images/Icon/calendar.png')}/>
+                                           source={require('../../../accset/images/Icon/calendar_red.png')}/>
                                 </View>
                                 <View style={{marginLeft: 10,}}>
                                     <Text style={{borderBottomWidth: 1, width: 130, padding: 2}}>dd/mm/yyyy </Text>
@@ -194,7 +80,7 @@ class SearchScreen extends Component {
                             }} style={{flexDirection: 'row'}}>
                                 <View style={{width: 20, height: 25, justifyContent: 'flex-end'}}>
                                     <Image style={{width: 20, height: 20,}}
-                                           source={require('../../../accset/images/Icon/calendar.png')}/>
+                                           source={require('../../../accset/images/Icon/ic_passage-of-time.png')}/>
                                 </View>
                                 <View style={{marginLeft: 10,}}>
                                     <Text style={{borderBottomWidth: 1, width: 130, padding: 2}}>0 night(s) </Text>
@@ -211,7 +97,7 @@ class SearchScreen extends Component {
                             }} style={{flexDirection: 'row', flex: 1}}>
                                 <View style={{width: 20, height: 25, justifyContent: 'flex-end'}}>
                                     <Image style={{width: 20, height: 20,}}
-                                           source={require('../../../accset/images/Icon/calendar.png')}/>
+                                           source={require('../../../accset/images/Icon/calendar_red.png')}/>
                                 </View>
                                 <View style={{marginLeft: 10,}}>
                                     <Text style={{borderBottomWidth: 1, width: 130, padding: 2}}>dd/mm/yyyy </Text>
@@ -221,52 +107,42 @@ class SearchScreen extends Component {
                         <View style={{flex: 1, marginLeft: 10}}>
                             <View><Text style={{color: '#8e8e8e', fontSize: 16, marginLeft: 30}}>Tour
                                 Guide</Text></View>
-                            <TouchableOpacity style={{flexDirection: 'row'}}>
+                            <TouchableOpacity onPress={() => {
+                                this._GetonPressButton()
+                            }} style={{flexDirection: 'row'}}>
                                 <View style={{width: 20, height: 25, justifyContent: 'flex-end'}}>
                                     <Image style={{width: 20, height: 20,}}
-                                           source={require('../../../accset/images/Icon/calendar.png')}/>
+                                           source={require('../../../accset/images/Icon/ic_slating-flag.png')}/>
                                 </View>
-                                <View style={{marginLeft: 10, borderBottomWidth: 1}}>
-                                    <Picker
-                                        style={{width: 130, height: 20}}
-                                        selectedValue={this.state.choosenLabel}
-                                        onValueChange={(itemValue, itemIndex) =>
-                                            this.setState({choosenLabel: itemValue, choosenindex: itemIndex})
-                                        }>
-                                        <Picker.Item label="Yes" value="yes"/>
-                                        <Picker.Item label="No" value="no"/>
-                                    </Picker>
+                                <View style={{marginLeft: 10,}}>
+                                    <Text style={{borderBottomWidth: 1, width: 130, padding: 2}}>No</Text>
                                 </View>
                             </TouchableOpacity>
                         </View>
+
                     </View>
                     <View style={{flexDirection: 'row', marginTop: 5,}}>
                         <View style={{flexDirection: 'row', flex: 1}}>
                             <View style={{width: 20, height: 45, justifyContent: 'flex-end'}}>
                                 <Image style={{width: 20, height: 20,}}
-                                       source={require('../../../accset/images/Icon/calendar.png')}/>
+                                       source={require('../../../accset/images/Icon/ic_min.png')}/>
                             </View>
                             <View style={{marginLeft: 10,}}>
-                                <Text style={{color: '#8e8e8e', fontSize: 16}}>Check-Out Date</Text>
+                                <Text style={{color: '#8e8e8e', fontSize: 16}}>Price</Text>
                                 <View style={{borderWidth: 1, borderRadius: 20}}>
-                                    <TextInput style={{width: 130, paddingLeft: 10, borderColor: 'gray', height: 40}}
-                                               keyboardType='numeric'
-                                               maxLength={6}
-                                    >0</TextInput>
+                                    <TextInput style={{width: 130, paddingLeft: 10, height: 40}}>0</TextInput>
                                 </View>
                             </View>
                         </View>
                         <View style={{flexDirection: 'row', flex: 1}}>
                             <View style={{width: 20, height: 45, justifyContent: 'flex-end'}}>
-                                <Image style={{width: 20, height: 20,}}
-                                       source={require('../../../accset/images/Icon/calendar.png')}/>
+                                <Image style={{width: 20, height: 20,justifyContent:'center'}}
+                                       source={require('../../../accset/images/Icon/ic_max.png')}/>
                             </View>
                             <View style={{marginLeft: 10}}>
                                 <Text style={{color: '#8e8e8e', fontSize: 16}}> </Text>
                                 <View style={{borderWidth: 1, borderRadius: 20}}>
-                                    <TextInput style={{width: 130, paddingLeft: 10, borderColor: 'gray', height: 40}}
-                                               keyboardType='numeric'
-                                               maxLength={6}>0</TextInput>
+                                    <TextInput style={{width: 130, paddingLeft: 10, height: 40}}>0</TextInput>
                                 </View>
                             </View>
                         </View>
@@ -295,12 +171,12 @@ class SearchScreen extends Component {
                             <Text style={{color: '#8e8e8e', fontSize: 16, marginLeft: 30}}>Guest</Text>
                         </View>
                         <TouchableOpacity onPress={() => {
-                            this.setState({isVisible: true})
+                            this._GetonPressButton()
                         }} style={{flexDirection: 'row', marginTop: 5,}}>
                             <View style={{flexDirection: 'row', flex: 1}}>
                                 <View style={{width: 20, height: 25, justifyContent: 'flex-end'}}>
                                     <Image style={{width: 20, height: 20,}}
-                                           source={require('../../../accset/images/Icon/calendar.png')}/>
+                                           source={require('../../../accset/images/Icon/ic_user.png')}/>
                                 </View>
                                 <View style={{marginLeft: 10,}}>
                                     <View style={{borderBottomWidth: 1, width: 300, flexDirection: 'row'}}>
@@ -312,9 +188,9 @@ class SearchScreen extends Component {
                             </View>
                         </TouchableOpacity>
                     </View>
-                    <View style={{marginTop: 10,}}>
+                    <View style={{ marginTop: 10,}}>
                         <View>
-                            <Text style={{color: '#8e8e8e', fontSize: 16, marginLeft: 30}}>Localhost</Text>
+                            <Text style={{color: '#8e8e8e', fontSize: 16, marginLeft:30}}>Localhost</Text>
                         </View>
                         <TouchableOpacity onPress={() => {
                             this._GetonPressButton()
@@ -322,7 +198,7 @@ class SearchScreen extends Component {
                             <View style={{flexDirection: 'row', flex: 1}}>
                                 <View style={{width: 20, height: 25, justifyContent: 'flex-end'}}>
                                     <Image style={{width: 20, height: 20,}}
-                                           source={require('../../../accset/images/Icon/calendar.png')}/>
+                                           source={require('../../../accset/images/Icon/ic_gps.png')}/>
                                 </View>
                                 <View style={{marginLeft: 10,}}>
                                     <View style={{borderBottomWidth: 1, width: 300, flexDirection: 'row'}}>
@@ -332,6 +208,7 @@ class SearchScreen extends Component {
                             </View>
                         </TouchableOpacity>
                     </View>
+
                 </View>
 
                 <View style={{flex: 1, justifyContent: 'flex-end'}}>
